4ac4932427f98be8630cb8a3d05c01aa1772376b
Maxuss <maxus.info.mail@gmail.com>
2023-12-05 23:15:04 +0300

    middleground walls

---
+++ Assets/Scripts/Utils/ParallaxBackground.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+namespace Utils
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ParallaxBackground: MonoBehaviour
+    {
+        private float length, startPos;
+        
+        [SerializeField]
+        private Transform cameraTransform;
+        [SerializeField]
+        private float transformModifier;
+
+        public void Start()
+        {
+            startPos = transform.position.x;
+            length = GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+
+        private void FixedUpdate()
+        {
+            var tmp = cameraTransform.position.x * (1 - transformModifier);
+            var dist = (cameraTransform.position.x * transformModifier);
+            transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+            if (tmp > startPos + length) startPos += length;
+            else if (tmp < startPos - length) startPos -= length;
+        }
+    }
+}
