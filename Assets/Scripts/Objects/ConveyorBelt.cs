aae0d8ca951b1739634d412afde742ae608031e8
Maxuss <maxus.info.mail@gmail.com>
2023-12-05 23:47:25 +0300

    working on conveyors

---
+++ Assets/Scripts/Objects/ConveyorBelt.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using Utils;
+
+namespace Objects
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class ConveyorBelt: MonoBehaviour
+    {
+        private bool _enabled = false;
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_enabled)
+                return;
+            
+            Vector3 oldPos = _rb.position;
+            _rb.position += transform.right.XY() * (2f * Time.fixedDeltaTime);
+            _rb.MovePosition(oldPos);
+        }
+    }
+}
