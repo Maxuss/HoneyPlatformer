4dee26e74d8610cc030b3f04f2f4085571868e2d
Maxuss <maxus.info.mail@gmail.com>
2023-12-08 12:29:38 +0300

    binary combinator

--- Assets/Scripts/Objects/BinaryCombinator.cs
+++ Assets/Scripts/Objects/BinaryCombinator.cs
@@ -1,9 +1,11 @@
+using System;
+using Program;
 using Program.Channel;
 using UnityEngine;
 
 namespace Objects
 {
-    public class BinaryCombinator: MonoBehaviour, IBiChannelReceiver
+    public class BinaryCombinator: MonoBehaviour, IBiChannelReceiver, IActionContainer
     {
         // TODO: program the operation
         [SerializeField]
@@ -64,11 +66,57 @@ namespace Objects
                 BinaryOperation.Or => _left || _right,
                 BinaryOperation.And => _left && _right,
                 BinaryOperation.Xor => _left ^ _right,
+                BinaryOperation.Implication => !_left || _right,
+                BinaryOperation.Equality => _left == _right,
                 _ => _state
             };
             
             _rx.ReceiveBool(transform, _state);
             _renderer.material.SetFloat(Output, _state ? 1f : 0f);
+        }
+
+        public string Name => "Логический Комбинатор";
+        public string Description => "Применяет бинарную операцию к двум сигналам на входе и выводит результат.";
+
+        public ActionInfo[] SupportedActions { get; } = new[]
+        {
+            new ActionInfo
+            {
+                ActionName = "OR",
+                ActionDescription = "Применяет логическую операцию ИЛИ"
+            },
+            new ActionInfo
+            {
+                ActionName = "AND",
+                ActionDescription = "Применяет логическую операцию И"
+            },
+            new ActionInfo
+            {
+                ActionName = "XOR",
+                ActionDescription = "Применяет логическую операцию Исключающего ИЛИ"
+            },
+            new ActionInfo
+            {
+                ActionName = "Импликация",
+                ActionDescription = "Применяет логическую операцию импликации."
+            },
+            new ActionInfo
+            {
+                ActionName = "Эквиваленция",
+                ActionDescription = "Применяет логическую операцию эквиваленции (приравнивания)."
+            }
+        };
+
+        public ProgrammableType Type { get; } = ProgrammableType.Emitter;
+        public ActionData SelectedAction { get; set; }
+        public void Begin(ActionData action)
+        {
+            var newOp = (BinaryOperation) Enum.ToObject(typeof(BinaryOperation), action.ActionIndex);
+            if (newOp == operation)
+                return;
+
+            operation = newOp;
+            RecalculateOutput();
         }
     }
 
@@ -76,6 +124,8 @@ namespace Objects
     {
         Or,
         And,
-        Xor
+        Xor,
+        Implication,
+        Equality
     }
 }
