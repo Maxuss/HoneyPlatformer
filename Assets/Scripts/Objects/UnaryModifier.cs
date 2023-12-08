545015dbf4f8bed05fd8497cdcc092949d40e207
Maxuss <maxus.info.mail@gmail.com>
2023-12-08 12:06:50 +0300

    unary modifier

--- Assets/Scripts/Objects/UnaryModifier.cs
+++ Assets/Scripts/Objects/UnaryModifier.cs
@@ -1,10 +1,11 @@
 using System;
+using Program;
 using Program.Channel;
 using UnityEngine;
 
 namespace Objects
 {
-    public class UnaryModifier: MonoBehaviour, IChannelReceiver
+    public class UnaryModifier: MonoBehaviour, IChannelReceiver, IActionContainer
     {
         [SerializeField]
         private UnaryOperator op;
@@ -32,10 +33,43 @@ namespace Objects
             _renderer.material.SetFloat(Output, _state ? 1f : 0f);
             _rx.ReceiveBool(transform, _state);
         }
-
+        
         public void ReceiveFloat(Transform source, float v)
         {
             // We don't support floats yet
+        }
+
+        public string Name => "Модификатор сигнала";
+        public string Description => "Модификатор применяет определенную операцию к получаемому сигналу и выводит его.";
+
+        public ActionInfo[] SupportedActions => new[]
+        {
+            new ActionInfo
+            {
+                ActionName = "Идентичность",
+                ActionDescription = "Выводит тот же сигнал, что и был получен на входе."
+            },
+            new ActionInfo
+            {
+                ActionName = "Отрицание",
+                ActionDescription = "Выводит сигнал, отрицательный полученному на входе."
+            }
+        };
+
+        public ProgrammableType Type => ProgrammableType.Emitter;
+        public ActionData SelectedAction { get; set; }
+        public void Begin(ActionData action)
+        {
+            var newOperator = (UnaryOperator) Enum.ToObject(typeof(UnaryOperator), action.ActionIndex);
+            if (op == newOperator)
+            {
+                return;
+            }
+
+            op = newOperator;
+            _state = !_state;
+            _renderer.material.SetFloat(Output, _state ? 1f : 0f);
+            _rx.ReceiveBool(transform, _state);
         }
     }
 
